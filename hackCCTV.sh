1fe64339c673b2967076e34dbd97dbeef759d46d
Jeeck007 <88170751+Jeeck007@users.noreply.github.com>
2021-07-30 13:04:06 +0700

    Add files via upload

---
+++ hackCCTV.sh
@@ -0,0 +1,50 @@
+#!/bi /sh/hack-cctv
+
+
+# tampilan
+figlet Hack Cctv | lolcat
+echo "=============================" | lolcat
+echo " Author : Mr.Jeeck"
+echo " Whatsapp : 081392505882"
+echo " Facebook : Jecko X Ganz"
+echo " Team : Sayber Seniper"
+echo "=============================" | lolcat
+echo
+echo " [1]. Mulai Hack"
+echo "==============================" | lolcat
+echo " [2]. Exit"
+echo "==============================" | lolcat
+echo
+read -p "pilih ==> " pill
+
+# data masukan
+if [ $pill = 1 ]; then
+echo "sedang menginstall"
+git clone https://github.com/MrVirusSpm-07/mobile
+sleep 2
+echo "sebentar lagi" | lolcat
+cd mobile
+echo "oke selesai" | lolcat
+sleep 1
+clear
+
+# tampilan 2
+figlet HACK-CCTV | lolcat
+echo "################################" | lolcat
+echo "[•] AUTHOR : Mr.Jeeck"
+echo "[•] TEAM : Syber Sniper"
+echo "[•] WHATSAPP : 081392505882"
+echo "[•] FACEBOOK : Jecko X Ganz"
+echo "################################" | lolcat
+echo
+sleep 1
+sh https_mrvirus_mobile.sh
+fi
+
+
+# data ke 2
+if [ $pill = 2 ]; then
+echo "Babay"
+exit
+if
+
